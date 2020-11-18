023adee1c0d4bddc066b0c45215c3d066d792303
ghzavala <guille.h.zavala@gmail.com>
2020-11-17 21:31:56 -0300

    Primer demo con handlebars

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require("express")
+const exphbs = require("express-handlebars")
 
 const app = express()
+
+app.engine("handlebars", exphbs())
+app.set("view engine", "handlebars")
 
 app.listen(2000)
 app.use( express.static( "public" ))
 
 app.get("/:seccion", (req, res) => {
     const { seccion } = req.params
-    res.writeHead(200, { "Content-Type" : "text/html"})
-    res.end(`<h1>Hola desde la sección <u>${seccion}</u> en HTML</h1>`)
+    
+    res.render('home')
 })
