822ee32bfb7b6725d63fdeb8fa6f4d293583cdfb
ghzavala <guille.h.zavala@gmail.com>
2020-11-19 20:43:33 -0300

    Agregando Axios para interactuar con APIs

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const exphbs = require("express-handlebars")
+const axios = require("axios")
 
 const app = express()
 
@@ -8,6 +9,15 @@ app.set("view engine", "handlebars")
 
 app.listen(2000)
 app.use( express.static( "public" ))
+
+app.get("/test", async (req, res) => {
+
+    const { data : peliculas } = await axios.get("http://localhost:1000/api/v1/pelicula")
+
+    console.log(peliculas)
+
+    res.end("Mira en la consola si hay datos de la API")
+})
 
 app.get("/:seccion?", (req, res) => {
     const { seccion } = req.params
@@ -18,3 +28,4 @@ app.get("/:seccion?", (req, res) => {
     
     res.render(vista, { titulo })
 })
+
